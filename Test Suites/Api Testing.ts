1220a02d99b8dd1678ebd83aed71ff8d2da8fd46
Muzammil-Ali-Khan <muzammilali1512@gmail.com>
2023-07-05 12:53:57 +0500

    feat: Api Testing

---
+++ Test Suites/Api Testing.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Api Testing</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>6ae8a068-2f7a-4f5e-a909-fc5fcbb05b7f</testSuiteGuid>
+   <testCaseLink>
+      <guid>49ef464e-99d4-4a87-95b6-a856cdc0d9f9</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Api Testing</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
